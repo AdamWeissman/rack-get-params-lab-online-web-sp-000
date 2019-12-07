e86ce55bccece890492da85fe6fbc520050d487c
Adam Weissman <adam@comedytragedyepic.com>
2019-12-07 12:28:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -16,6 +17,8 @@ class Application
     else
       resp.write "Path Not Found"
     end
+    
+    if req.path.match(/cart/)
 
     resp.finish
   end

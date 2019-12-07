038f229ff0ce1357de00db09da5ed5d1150c9099
Adam Weissman <adam@comedytragedyepic.com>
2019-12-07 12:52:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,25 +16,25 @@ class Application
       end
     elsif pm.call(/search/)
       search_term = req.params["q"]
-      resp.write handle_search(search_term)
+      rw handle_search(search_term)
     elsif pm.call(/cart/)
       if @@cart.empty?
-        resp.write("Your cart is empty")
+        rw("Your cart is empty")
       else
         @@cart.each do |item|
-          resp.write "#{item}\n"
+          rw "#{item}\n"
         end
       end
     elsif pm.call(/add/)
       item_param = req.params["item"]
       if @@items.include?(item_param)
         @@cart << item_param
-        resp.write("added #{item_param}")
+        rw("added #{item_param}")
       else
-        resp.write("We don't have that item")
+        rw("We don't have that item")
       end
     else
-      resp.write "Path Not Found"
+      rw "Path Not Found"
     end
 
     resp.finish
